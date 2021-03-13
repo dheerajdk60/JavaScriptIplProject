4cc9742ca6115d103625ebee960fb96475c1e6a7
dheeraj kumar <dheeraj.kumar@mountblue.tech>
2021-03-14 02:00:17 +0530

    all solved refactoring remaining

--- main.js
+++ main.js
@@ -40,33 +40,10 @@ const fs = require('fs');
 
 var matchesList = getMatchesList();
 var deliveriesList = getDeliveriesList();
-
-//matchesPlayedPerYear();
+matchesPlayedPerYear();
 matchesWonPerTeam();
-
-function matchesWonPerTeam()
-{
-    var matchesWonPerTeam = {};
-    matchesList.forEach(match => {
-        matchesWonPerTeam[match.winner] = (matchesWonPerTeam[ match.winner] == undefined) ? 1 : matchesWonPerTeam[ match.winner] + 1;
-    });
-    for (key in matchesWonPerTeam) {
-        if(key.length!=0)
-        {
-            console.log(key + "  " + matchesWonPerTeam[key]);
-        }
-    }
-}
-
-function matchesPlayedPerYear() {
-    var matchesPlayedPerYear = {};
-    matchesList.forEach(match => {
-        matchesPlayedPerYear["" + match.date.getFullYear()] = (matchesPlayedPerYear["" + match.date.getFullYear()] == undefined) ? 1 : matchesPlayedPerYear["" + match.date.getFullYear()] + 1;
-    });
-    for (key in matchesPlayedPerYear) {
-        console.log(key + "  " + matchesPlayedPerYear[key]);
-    }
-}
+extraRunsConcededPerTeamIn2016();
+topEconomicalBowlersIn2015();
 function getMatchesList() {
     var matchesLines = fs.readFileSync("matches.csv").toString().split("\n");
     matchesLines.shift();
@@ -134,4 +111,101 @@ function getDeliveriesList() {
         };
         deliveriesList.push(delivery);
     });
+    return deliveriesList;
+}
+function matchesPlayedPerYear() {
+    console.log("Number of matches played per year of all the years in IPL");
+    var matchesPlayedPerYear = {};
+    matchesList.forEach(match => {
+        matchesPlayedPerYear["" + match.date.getFullYear()] = (matchesPlayedPerYear["" + match.date.getFullYear()] == undefined) ? 1 : matchesPlayedPerYear["" + match.date.getFullYear()] + 1;
+    });
+    for (key in matchesPlayedPerYear) {
+        console.log(key + "  " + matchesPlayedPerYear[key]);
+    }
+}
+function matchesWonPerTeam()
+{
+    console.log("Number of matches won of all teams over all the years of IPL");
+    var matchesWonPerTeam = {};
+    matchesList.forEach(match => {
+        matchesWonPerTeam[match.winner] = (matchesWonPerTeam[ match.winner] == undefined) ? 1 : matchesWonPerTeam[ match.winner] + 1;
+    });
+    for (key in matchesWonPerTeam) {
+        if(key.length!=0)
+        {
+            console.log(key + "  " + matchesWonPerTeam[key]);
+        }
+    }
+}
+function extraRunsConcededPerTeamIn2016()
+{
+    console.log("For the year 2016 get the extra runs conceded per team");
+    var teamNameWithRunsConceded = {};
+    deliveriesList.forEach(function (delivery) {
+            if (matchesList[delivery.id - 1].date.getFullYear() == 2016) {
+               if( teamNameWithRunsConceded[delivery.bowlingTeam]==undefined) 
+               {
+                    teamNameWithRunsConceded[delivery.bowlingTeam]=Number(delivery.extraRuns) ;
+               }
+               else{
+                teamNameWithRunsConceded[delivery.bowlingTeam]+=Number(delivery.extraRuns);
+               }
+            }
+        });
+    for (key in  teamNameWithRunsConceded) {
+        console.log(key + "  " + teamNameWithRunsConceded[key]);
+    }
+}
+function topEconomicalBowlersIn2015()
+{
+    console.log("For the year 2015 get the top economical bowlers");
+    var topEconomicalBowlers = {};
+    deliveriesList.forEach(function (delivery) {
+            if (matchesList[delivery.id - 1].date.getFullYear() == 2015) {
+                var runs=getInt(delivery.noBallRuns)+getInt(delivery.extraRuns)+getInt(delivery.batsmanRuns)+getInt(delivery.wideRuns);
+                if(topEconomicalBowlers[delivery.bowler]==undefined)
+                {
+                    topEconomicalBowlers[delivery.bowler]=[1,runs];
+                }
+                else{
+                    topEconomicalBowlers[delivery.bowler][0]++;
+                    topEconomicalBowlers[delivery.bowler][1]+=runs;
+                }
+            }
+        });
+        var bowlersNames=[];
+        var bowlersEconomies=[];
+        for(bowlerName in topEconomicalBowlers)
+        {
+            var bowlerEconomyDetails=topEconomicalBowlers[bowlerName];
+            var economy=bowlerEconomyDetails[1]/(bowlerEconomyDetails[0]/6.0);
+            economy=Number(economy.toFixed(3));
+            bowlersNames.push(bowlerName);
+            bowlersEconomies.push(economy);
+        }
+        for(var i=0;i<bowlersEconomies.length;i++)
+        {
+            for(var j=i+1;j<bowlersEconomies.length;j++)
+            {
+                if(bowlersEconomies[i]>bowlersEconomies[j])
+                {
+                    var temp=bowlersEconomies[i];
+                    bowlersEconomies[i]=bowlersEconomies[j];
+                    bowlersEconomies[j]=temp;
+                    temp=bowlersNames[i];
+                    bowlersNames[i]=bowlersNames[j];
+                    bowlersNames[j]=temp;
+                }
+            }   
+        }
+        for(var i=0;i<bowlersEconomies.length;i++)
+        {
+            console.log(bowlersEconomies[i]+"     "+bowlersNames[i]);
+        }
+}
+function getInt(num)
+{
+    if(Number(num)==NaN)
+    return 0;
+    return Number(num);
 }
