b9879c32d53ef0c4ea100c8db1dea8643d3af313
dheeraj kumar <dheeraj.kumar@mountblue.tech>
2021-03-13 17:41:46 +0530

    added delivery constant indices

--- main.js
+++ main.js
@@ -15,12 +15,38 @@ const MATCH_PLAYER_OF_MATCH=13;
 const MATCH_VENUE=14;
 const MATCH_UMPIRE1=15;
 const MATCH_UMPIRE2=16;
+const DELIVERY_ID=0;
+const DELIVERY_INNING=1;
+const DELIVERY_BATTING_TEAM=2;
+const DELIVERY_BOWLING_TEAM=3;
+const DELIVERY_OVER=4;
+const DELIVERY_BALL=5;
+const DELIVERY_BATSMAN=6;
+const DELIVERY_NON_STRIKER=7;
+const DELIVERY_BOWLER=8;
+const DELIVERY_IS_SUPER_OVER=9;
+const DELIVERY_WIDE_RUNS=10;
+const DELIVERY_BYE_RUNS=11;
+const DELIVERY_LEG_BYE_RUNS=12;
+const DELIVERY_NO_BALL_RUNS=13;
+const DELIVERY_PENALTY_RUNS=14;
+const DELIVERY_BATSMAN_RUNS=15;
+const DELIVERY_EXTRA_RUNS=16;
+const DELIVERY_TOTAL_RUNS=17;
+const DELIVERY_PLAYER_DISMISSED=18;
+const DELIVERY_DISMISSAL_KIND=19;
+const DELIVERY_FIELDER=20;
 const fs=require('fs');
 var matchesLines=fs.readFileSync("matches.csv").toString().split("\n");
+var deliveriesLines=fs.readFileSync("deliveries.csv").toString().split("\n");
 var matchesList=[];
+var deliveriesList=[];
+matchesLines.shift();
+matchesLines.pop();
 matchesLines.forEach(line => {
     var fields=line.split(",");
     var adjustingIndex=0;
+    var dateValues=fields[MATCH_DATE].split("-");
     if(line.includes("\""))
     {
         adjustingIndex=1;
@@ -29,7 +55,7 @@ matchesLines.forEach(line => {
         id:fields[MATCH_ID],
         season:fields[MATCH_SEASON],
         city:fields[MATCH_CITY],
-        date:fields[MATCH_DATE],
+        date:new Date(dateValues[0],dateValues[1],dateValues[2]),
         team1:fields[MATCH_TEAM1],
         team2:fields[MATCH_TEAM2],
         tossWinnner:fields[MATCH_TOSS_WINNER],
@@ -46,4 +72,3 @@ matchesLines.forEach(line => {
     };
     matchesList.push(match);
 });
-console.log(matchesList);
